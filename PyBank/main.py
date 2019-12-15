bb6e8a729bbfbfb077a14d4c7005e6439b320b1f
KP Dorsey <dorsey_k17@hotmail.com>
2019-12-14 20:19:59 -0600

    Added pseudocode

---
+++ PyBank/main.py
@@ -0,0 +1,23 @@
+# Read in file
+
+
+# Analyze
+## The total number of months included in the dataset
+
+
+## The net total amount of "Profit/Losses" over the entire period
+
+
+## The average of the changes in "Profit/Losses" over the entire period
+
+
+## The greatest increase in profits (date and amount) over the entire period
+
+
+## The greatest decrease in losses (date and amount) over the entire period
+
+
+# Print analysis to terminal
+
+
+# Save analysis to file
