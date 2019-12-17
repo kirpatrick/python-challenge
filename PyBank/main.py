2bb046add04f08aff850025ec00a46af706f547c
KP Dorsey <dorsey_k17@hotmail.com>
2019-12-17 01:41:52 -0600

    Refactored main and test files for code readability

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,83 +2,87 @@ import os
 import csv
 import statistics
 
-####################################################################
-### References
-### 11-Stu_UdemyZip
-### netfliz.py
-### comprehensions.py
-### read_csv.py
-####################################################################
-
 # Read in 'budget_data.csv'
-# Note: main.py is at the same level as the data folder,
-# so reference same level in dir tree; '.'
 budget_csv = os.path.join('.','data', 'budget_data.csv')
 
-# Initialize variables
-number_of_months = 0
-profits = 0.00
-losses = 0.00
-total_profits = 0.00
-initial_monthly_value = 0.00
-current_change = 0.00
-current_monthly_value = []
-
-# Analyze
+# Create dictionary to store budget data
+myBudgetDataDictionary = {
+    "month-year": [],
+    "profit": []
+}
 
 ### Open and Read budget_csv
 with open(budget_csv, newline='') as csvfile:
 
-    #### Read budget_csv
     #### CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
 
     #### Read in header and skip in dataset
     csv_header = next(csvreader)
 
-    #### Capture row count
+    #### Save budget data to myBudgetDataDictionary
     for row in csvreader:
-        number_of_months = number_of_months + 1
-        current_monthly_value.append(float(row[1]))
+        myBudgetDataDictionary["month-year"].append(row[0])
+        myBudgetDataDictionary["profit"].append(float(row[1]))
 
-        if float(row[1]) < 0:
-            losses = losses + float(row[1])
-        else:
-            profits = profits + float(row[1])
-
-### End budget_csv read
-
-### Print total number of months
-print(number_of_months)
+### Print total number of months (records)
+print(len(myBudgetDataDictionary["month-year"]))
 
 ## The net total amount of "Profit/Losses" over the entire period
-total_profits = profits + losses
-print(round(total_profits))
+print(round(sum(myBudgetDataDictionary["profit"])))
 
 ## The average of the changes in "Profit/Losses" over the entire period
-# print(average_change_over_time)
+### Store profits for the first recorded month
+initial_monthly_value = myBudgetDataDictionary["profit"][0]
 
-initial_monthly_value = current_monthly_value[0]
-# print(initial_monthly_value)
-# print(len(monthly_values))
+### Create dictionary to store changes in profit/loss
+monthlyChangeDictionary = {
+    "month-year": [],
+    "profit": []
+}
 
-monthly_change = []
+### iterate through all budget data in myBudgetDataDictionary
+for i in range(1,len(myBudgetDataDictionary["profit"])):
 
-for i in range(1,len(current_monthly_value)):
-    monthly_change.append(current_monthly_value[i] - initial_monthly_value)
-    initial_monthly_value = current_monthly_value[i]
+    #### Store first month's comparison data into myBudgetDataDictionary.  E.g. currentMonth - previousMonth
+    #### Capture month-year
+    monthlyChangeDictionary["month-year"].append(myBudgetDataDictionary["month-year"][i])
+    #### Capture change in USD
+    monthlyChangeDictionary["profit"].append(myBudgetDataDictionary["profit"][i] - initial_monthly_value)
 
-print(round(statistics.mean(monthly_change),2))
-# print(f"number of rows in average_change_over_time:  {loop_count}")
-# print(initial_monthly_value)
+    #### Iterate the initial value for next comparison
+    initial_monthly_value = myBudgetDataDictionary["profit"][i]
+
+### Return the average monthly change
+print(round(statistics.mean(monthlyChangeDictionary["profit"]),2))
 
 ## The greatest increase in profits (date and amount) over the entire period
-print(round(max(monthly_change)))
+### Given the index for max profit increase in monthlyChangeDictionary, return the month-year
+print(monthlyChangeDictionary["month-year"][monthlyChangeDictionary["profit"].index(max(monthlyChangeDictionary["profit"]))])
+### Return max profit increase USD
+print(round(max(monthlyChangeDictionary["profit"])))
 
 ## The greatest decrease in losses (date and amount) over the entire period
-print(round(min(monthly_change)))
+### Given the index for max profit decrease in monthlyChangeDictionary, return the month-year
+print(monthlyChangeDictionary["month-year"][monthlyChangeDictionary["profit"].index(min(monthlyChangeDictionary["profit"]))])
+### Return max profit decrease USD
+print(round(min(monthlyChangeDictionary["profit"])))
 
 # Print analysis to terminal
-
+## Need to format print statements
 
 # Save analysis to file
+output_file = os.path.join('.','data', 'output.csv')
+
+with open(output_file, "w", newline="") as datafile:
+    writer = csv.writer(datafile)
+
+    writer.writerow(
+        [
+            "Title",
+            "Price",
+            "Subscriber Count",
+            "Number of Reviews", 
+            "Course Length"
+            ]
+        )
--- PyBank/test.py
+++ PyBank/test.py
@@ -8,7 +8,6 @@ import statistics
 ### netfliz.py
 ### comprehensions.py
 ### read_csv.py
-### dictionaries.py
 ####################################################################
 
 # Read in 'budget_data.csv'
@@ -17,14 +16,13 @@ import statistics
 budget_csv = os.path.join('.','data', 'budget_data.csv')
 
 # Initialize variables
+number_of_months = 0
+profits = 0.00
+losses = 0.00
+total_profits = 0.00
 initial_monthly_value = 0.00
+current_change = 0.00
 current_monthly_value = []
-
-# dictionary
-myDictionary = {
-    "month-year": [],
-    "profit": []
-}
 
 # Analyze
 
@@ -40,54 +38,45 @@ with open(budget_csv, newline='') as csvfile:
 
     #### Capture row count
     for row in csvreader:
+        number_of_months = number_of_months + 1
         current_monthly_value.append(float(row[1]))
 
-        myDictionary["month-year"].append(row[0])
-        myDictionary["profit"].append(float(row[1]))
+        if float(row[1]) < 0:
+            losses = losses + float(row[1])
+        else:
+            profits = profits + float(row[1])
+
+### End budget_csv read
 
 ### Print total number of months
-# print(len(current_monthly_value))
-print(len(myDictionary["month-year"]))
+print(number_of_months)
 
 ## The net total amount of "Profit/Losses" over the entire period
-# print(round(sum(current_monthly_value)))
-print(round(sum(myDictionary["profit"])))
+total_profits = profits + losses
+print(round(total_profits))
 
 ## The average of the changes in "Profit/Losses" over the entire period
+# print(average_change_over_time)
 
-# initial_monthly_value = current_monthly_value[0]
-initial_monthly_value = myDictionary["profit"][0]
+initial_monthly_value = current_monthly_value[0]
+# print(initial_monthly_value)
+# print(len(monthly_values))
 
 monthly_change = []
 
-monthlyChangeDictionary = {
-    "month-year": [],
-    "profit": []
-}
+for i in range(1,len(current_monthly_value)):
+    monthly_change.append(current_monthly_value[i] - initial_monthly_value)
+    initial_monthly_value = current_monthly_value[i]
 
-for i in range(1,len(myDictionary["profit"])):
-
-    monthly_change.append(myDictionary["profit"][i] - initial_monthly_value)
-    monthlyChangeDictionary["month-year"].append(myDictionary["month-year"][i])
-    monthlyChangeDictionary["profit"].append(myDictionary["profit"][i] - initial_monthly_value)
-
-    initial_monthly_value = myDictionary["profit"][i]
-
-# print(round(statistics.mean(monthly_change),2))
-print(round(statistics.mean(monthlyChangeDictionary["profit"]),2))
+print(round(statistics.mean(monthly_change),2))
+# print(f"number of rows in average_change_over_time:  {loop_count}")
+# print(initial_monthly_value)
 
 ## The greatest increase in profits (date and amount) over the entire period
-# print(round(max(monthly_change)))
-print(round(max(monthlyChangeDictionary["profit"])))
-# Return index for month-year of greatest decrease
-print(monthlyChangeDictionary["profit"].index(max(monthlyChangeDictionary["profit"])))
-print(monthlyChangeDictionary["month-year"][monthlyChangeDictionary["profit"].index(max(monthlyChangeDictionary["profit"]))])
+print(round(max(monthly_change)))
 
 ## The greatest decrease in losses (date and amount) over the entire period
-print(round(min(monthlyChangeDictionary["profit"])))
-# Return index for month-year of greatest decrease
-print(monthlyChangeDictionary["profit"].index(min(monthlyChangeDictionary["profit"])))
-print(monthlyChangeDictionary["month-year"][monthlyChangeDictionary["profit"].index(min(monthlyChangeDictionary["profit"]))])
+print(round(min(monthly_change)))
 
 # Print analysis to terminal
 
