e187c57ba234cbe91ef39df97afbe0a7445ddef9
KP Dorsey <dorsey_k17@hotmail.com>
2019-12-21 23:20:35 -0600

    Word count works

---
+++ ExtraContent/PyParagraph/main_PyParagraph.py
@@ -0,0 +1,43 @@
+from collections import Counter
+
+# Store the file paths associated with the files
+# file = './raw_data/paragraph_1.txt'
+# file = './raw_data/paragraph_2.txt'
+file = './raw_data/paragraph_3.txt'
+
+# Open the file in "read" mode ('r') and store the contents in the variable "text"
+with open(file, 'r') as text:
+
+    # This stores a reference to a file stream
+    # print(text)
+
+    # Store all of the text inside a variable called "lines"
+    # lines = text.read()
+
+    # Print the contents of the text file
+    # print(lines)
+    wordCount = Counter(text.read().split())
+    sentenceCount = Counter(text.read().split("."))
+
+# print(wordcount)
+
+# Print Results to terminal
+print("Paragraph Analysis")
+print("---------------------------------")
+# Assess the passage for each of the following:
+
+# Approximate word count
+print(f"Approximate Word Count:  {sum(wordCount.values())}")
+#################################################################################
+
+# Approximate sentence count
+print(f"Approximate Sentence Count:  {sum(sentenceCount.values())}")
+#################################################################################
+
+# Approximate letter count (per word)
+
+#################################################################################
+
+# Average sentence length (in words)
+
+#################################################################################
